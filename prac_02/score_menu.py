646e0774d44ee82b3425b38a596dd7073f2eecae
Isaac <shiheng.wang@my.jcu.edu.au>
2025-05-27 18:00:38 +0800

    Add score menu program

---
+++ prac_02/score_menu.py
@@ -0,0 +1,51 @@
+MENU = """
+(G)et a valid score (must be 0-100 inclusive)
+(P)rint result
+(S)how stars
+(Q)uit
+"""
+
+def main():
+    valid_score = None
+    print(MENU)
+    choice = input("Make a choice: ")
+    while choice != "Q":
+        if choice == "G":
+            valid_score = get_score()
+        elif choice == "P":
+            if valid_score is None:
+                print("Choose G first")
+            else:
+                # valid_score = get_score()
+                print(get_result(valid_score))
+        elif choice == "S":
+            if valid_score is None:
+                print("Choose G first")
+            else:
+                # valid_score = get_score()
+                print("*" * valid_score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input("Make a choice: ")
+    print("farewell")
+
+
+def get_score():
+    score = int(input("Enter a score: "))
+    while score < 0 or score > 100:
+        print("Invalid score.")
+        score = int(input("Enter a score: "))
+    return score
+
+def get_result(score):
+    if 0 > score or score> 100:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
+main()
