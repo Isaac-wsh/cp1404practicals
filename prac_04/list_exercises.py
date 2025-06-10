701eb610047f01ee7cf74c5b9692e010eaf6c2f4
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-10 13:21:55 +0800

    Print the last number in the list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,7 +10,7 @@ numbers.append(number3)
 numbers.append(number4)
 numbers.append(number5)
 print(f"The first number is {numbers[0]}")
-# print(f"The last number is {numbers[-1]}")
+print(f"The last number is {numbers[-1]}")
 # print(f"The smallest number is {min(numbers)}")
 # print(f"The largest number is {max(numbers)}")
 # def calculate_average(number):
