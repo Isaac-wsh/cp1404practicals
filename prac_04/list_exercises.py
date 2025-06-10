0970ec63cb65a49995d90441418e3f790557c9a4
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-10 13:22:41 +0800

    Print the maximum value in a list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,7 +12,7 @@ numbers.append(number5)
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
-# print(f"The largest number is {max(numbers)}")
+print(f"The largest number is {max(numbers)}")
 # def calculate_average(number):
 #     """calculate the average of numbers in the list"""
 #     return sum(number) / len(number)
