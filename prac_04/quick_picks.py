1431fc0440eb082aa507e0702fb183d78e22fb84
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-13 13:52:01 +0800

    Make values stored as CONSTANTS

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,5 +1,9 @@
 import random
 
+PICK_NUMBER = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
 def main():
     pick_count = int(input("How many quick picks? "))
     count = 0
@@ -13,8 +17,8 @@ def main():
 def quick_pick():
     """Random pick 6 numbers"""
     numbers = []
-    while len(numbers) < 6:
-        picked_number = random.randint(1,45)
+    while len(numbers) < PICK_NUMBER:
+        picked_number = random.randint(MIN_NUMBER,MAX_NUMBER)
         if picked_number not in numbers:
             numbers.append(picked_number)
     numbers.sort()
