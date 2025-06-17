58368d7f0dc282e12abfdd3b3162a9a5ccd3bb11
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-18 03:59:22 +0800

    Create a dictionary with all keys in lowercase

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,6 +10,10 @@ COLOR_TO_CODE = {"Absolute Zero": "#0048ba",
                  "Arylide Yellow": "#e9d66b"}
 print(COLOR_TO_CODE)
 
+lower_color_dict = {}
+for name in COLOR_TO_CODE:
+    lower_color_dict[name.lower()] = name
+
 input_name = input("Enter color name: ")
 while input_name != "":
     name_separated = input_name.casefold().split(",")
