ec852529e982c7c8b9fc6a48af2053c775bec162
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-18 02:14:10 +0800

    Change the program so lowercase inputs also work to show the state names.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,15 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    code_upper = state_code.upper()
+    if code_upper in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[code_upper])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
