b445d7205ffcc011e096448c7e177bf4b58755a9
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-18 14:52:53 +0800

    Count the number of times each word appears in a sentence

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,3 +6,8 @@ Actual:   32 minutes
 
 text = input("Enter the text: ")
 words = text.split()
+word_count = {}
+
+for word in words:
+    word = word.lower()
+    word_count[word] = word_count.get(word,0) + 1
