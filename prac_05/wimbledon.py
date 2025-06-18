89411d1085edaa6a83a3bd4d2b60c6f75d3b468e
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-18 16:55:03 +0800

    Create read_data function to read the file and return the champion and country

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,3 +6,17 @@ Actual:    minutes
 
 def main():
     filename = "wimbledon.csv"
+
+def read_data(filename):
+    """Read the file and return the champion and country"""
+    import csv
+    champions = []
+    countries = set()
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.DictReader(in_file)
+        for row in reader:
+            champion = row["Champion"]
+            country = row["Country"]
+            champions.append(champion)
+            countries.add(country)
+    return champions, countries
