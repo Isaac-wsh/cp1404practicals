b671cd004e6faf9d3432372f61c0e39201f9bd07
Isaac <shiheng.wang@my.jcu.edu.au>
2025-06-18 14:58:01 +0800

    Sort the count and print the result

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -13,3 +13,6 @@ for word in words:
     word_count[word] = word_count.get(word,0) + 1
 
 max_len = max(len(word) for word in word_count)
+sorted_count = sorted(word_count)
+for word in sorted_count:
+    print(f"{word:<{max_len}} : {word_count[word]}")
