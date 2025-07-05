d9c65ba1f73787ca5e9ab8ee69f462a460ca5ee9
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-05 16:35:50 +0800

    Define __init__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,9 @@ Actual:    minutes
 """
 
 class ProgrammingLanguage:
+    def __init__(self, language, typing, reflection, year):
+        """Like most init functions, create the fields and set them to the parameters passed in"""
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
