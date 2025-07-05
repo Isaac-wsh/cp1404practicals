b47cfb87032487d74fedd355d2854c7c6cbf0d9a
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-05 23:41:06 +0800

    Using User input as data

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,17 +4,17 @@ def main():
     """Use Guitar class"""
     print("My guitars!")
     guitars = []
-    # name = input("Name: ")
-    # while name != "":
-    #     year = int(input("Year: "))
-    #     cost = float(input("Cost: "))
-    #     guitar = Guitar(name, year, cost)
-    #     guitars.append(guitar)
-    #     print(guitar, "added")
-    #     name = input("Name: ")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(guitar, "added")
+        name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     if guitars:
         print("These are my guitars:")
         for i, guitar in enumerate(guitars, 1):
