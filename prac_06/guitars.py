42097ea067ee448591c7e06446976a1928852282
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-05 23:40:23 +0800

    Using Python's ternary operator to do

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,16 +15,14 @@ def main():
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    if len(guitars) > 0:
+    if guitars:
         print("These are my guitars:")
-        i = 1
-        for guitar in guitars:
+        for i, guitar in enumerate(guitars, 1):
             if guitar.is_vintage():
-                vintage = " (vintage)"
+                vintage_string = " (vintage)"
             else:
-                vintage = ""
-            print("Guitar " + str(i) + ": " + guitar.name + " (" + str(guitar.year) + "), worth $" + str(round(guitar.cost, 2)) + vintage)
-            i += 1
+                vintage_string = ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 main()
 
