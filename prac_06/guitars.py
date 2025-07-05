c746c82dd631fb03518f0035d665ddabf07c86d1
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-05 23:24:19 +0800

    Complete the input part, let user input guitar details

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,1 +1,17 @@
 from guitar import Guitar
+
+def main():
+    """Use Guitar class"""
+    print("My guitars!")
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(guitar, "added")
+        name = input("Name: ")
+
+
+
