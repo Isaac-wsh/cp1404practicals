2f45080c5d0e3098c49b634a61a9dfa6ccbf58f7
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-08 13:41:41 +0800

    Write the save_guitars function to Write list of Guitar to file.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,4 +1,6 @@
 from guitar import Guitar
+from prac_03.capitalist_conrad import out_file
+
 
 def main():
     """Load guitars from file, sort them by year, and display the list."""
@@ -6,6 +8,16 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name:")
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
 
 def load_guitars(filename):
     """Read guitars from file and return list of Guitar."""
@@ -19,5 +31,12 @@ def load_guitars(filename):
         guitars.append(Guitar(name, year, cost))
     in_file.close()
     return guitars
+
+def save_guitars(filename, guitars):
+    """Write list of Guitar to file."""
+    out_file = open(filename, "w")
+    for guitar in guitars:
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
 
 main()
