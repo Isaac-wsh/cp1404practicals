c6b436c61e04352c0b66083db4190592ce1c1b54
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-08 21:35:33 +0800

    Fix __init__ constructor

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class Project:
     """Used to represent data of project class"""
 
-    def __int__(self, name, start_time, priority, estimate, completion):
+    def __init__(self, name, start_time, priority, estimate, completion):
         """Initialize the project object"""
         self.name = name
         self.start_time = datetime.strptime(start_time, "%d/%m/%Y").date()
@@ -21,3 +21,8 @@ class Project:
         return (f"{self.name}, start: {self.start_time.strftime("%d/%m/%Y")},"
                 f"priority {self.priority}, estimate:${self.estimate:.2f},"
                 f"completion:{self.completion}%")
+
+    def __lt__(self, other):
+        """Automatically sort Project objects by priority"""
+        return self.priority < other.priority
+
