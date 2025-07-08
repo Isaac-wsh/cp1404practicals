f73276fa66bb1cfad26f0aa06a266057de702f29
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-08 21:24:33 +0800

    Correct spelling errors Fix import errors

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,8 +3,7 @@ Estimate: 40 minutes
 Actual:   minutes
 """
 
-import datetime
-
+from datetime import datetime
 
 class Project:
     """Used to represent data of project class"""
@@ -12,7 +11,7 @@ class Project:
     def __int__(self, name, start_time, priority, estimate, completion):
         """Initialize the project object"""
         self.name = name
-        self.start_time = datatime.strptime(start_time, "%d/%m/%Y").date()
+        self.start_time = datetime.strptime(start_time, "%d/%m/%Y").date()
         self.priority = int(priority)
         self.estimate = float(estimate)
         self.completion = int(completion)
