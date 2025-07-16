0db211ee218606545ac7759fe0ef138a451c7932
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-16 13:09:24 +0800

    Create the build function to build the Kivy GUI and add a Label for each name.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -8,5 +8,13 @@ class DynamicLabelsApp(App):
         super().__init__(**kwargs)
         self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
 
+    def build(self):
+        """Build the Kivy GUI and add a Label for each name."""
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            label = Label(text=name)
+            self.root.ids.main.add_widget(label)
+        return self.root
 
+DynamicLabelsApp().run()
 
