1b119befc1aae2ab6130d6944d291323c25d5c78
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-22 22:44:39 +0800

    Create add function to add a musician to the band.

--- prac_09/band.py
+++ prac_09/band.py
@@ -4,3 +4,7 @@ class Band:
         self.name = name
         self.musicians = []
 
+    def add(self, musician):
+        """Add a musician to the band"""
+        self.musicians.append(musician)
+
