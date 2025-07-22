7992ce353590bf935b2c271fdd6d09bcd72f2e6a
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-22 14:01:04 +0800

    Create a function to calculate the fare.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,3 +7,9 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Calculate the fare."""
+        base_fare = super().get_fare()
+        return base_fare + self.flagfall
+
