d3e92f750034d656afdc8658c21cf31f64d35049
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-22 12:57:38 +0800

    Create drive function to show that drive the car if a random number is less than the car's reliability.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,4 +1,5 @@
 from prac_09.car import Car
+import random
 
 class UnreliableCar(Car):
     """Make our own derived class for an UnreliableCar that inherits from Car"""
@@ -6,3 +7,11 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car"""
+        random_chance = random.uniform(0, 100)
+        if random_chance < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
