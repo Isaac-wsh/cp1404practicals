c70b61fceb390f8f9f7ad6af401eba542b522de5
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-23 00:05:09 +0800

    Create display_taxis function to display the list of taxis with their index.

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,19 @@
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Call Taxi and SilverServiceTaxi and output the results"""
+    print("Let's drive!")
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total_bill = 0.0
+    print("q)uit, c)hoose taxi, d)rive")
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+
+def display_taxis(taxis):
+    """Display the list of taxis with their index."""
+    print("Taxis available: ")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
