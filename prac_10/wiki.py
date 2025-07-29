c30d9ae0c3411d018c80d38fd037d5dd158f1a45
Isaac <shiheng.wang@my.jcu.edu.au>
2025-07-29 12:29:23 +0800

    Write a program that prompts the user for a page title or search phrase, then prints some details of that page.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,3 +1,22 @@
 import wikipedia
 
 wikipedia.set_lang("en")
+
+def main():
+    while True:
+        topic = input("Enter page title: ").strip()
+        if topic == "":
+            print("Goodbye!")
+            break
+        try:
+            page = wikipedia.page(title=topic, auto_suggest=False)
+        except wikipedia.DisambiguationError as e:
+            print(f"Ambiguous topic. Options: {e.options[:5]}")
+            continue
+        except wikipedia.PageError:
+            print("Page not found. Try another id.")
+            continue
+        print(f"\nTitle: {page.title}")
+        print(f"URL: {page.url}")
+        print(f"Summary:\n{wikipedia.summary(page.title, sentences=2)}\n")
+main()
